92e3a9a4d1baa10032ea2c8ad86cbaa946e4d73c
Raúl Rodríguez Pearson <raul.rodriguez.pearson@gmail.com>
2018-06-24 18:01:09 +0800

    Add typography.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,13 @@ module.exports = {
   siteMetadata: {
     title: 'Data Manager',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-typography',
+      options: {
+        pathToConfigModule: 'src/utils/typography',
+      },
+    },
+  ],
 }
---
+++ src/utils/typography.js
@@ -0,0 +1,6 @@
+import Typography from "typography";
+import elkGlenTheme from "typography-theme-elk-glen";
+
+const typography = new Typography(elkGlenTheme);
+
+export default typography;
